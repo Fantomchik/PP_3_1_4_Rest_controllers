657f875d0d58d8d765bf0af0ef19444515e76de4
Fantomchik <Lordfantom455@gmail.com>
2025-04-14 18:04:07 +0300

    init

---
+++ src/main/java/ru/kata/spring/boot_security/demo/entityes/User.java
@@ -0,0 +1,127 @@
+package ru.kata.spring.boot_security.demo.entityes;
+
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username")
+    private String userName;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email")
+    private String email;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @Fetch(FetchMode.JOIN)
+    @JoinTable(
+            name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles;
+
+    public User() {
+    }
+
+    public User(String userName, String password, String email, Set<Role> roles) {
+        this.userName = userName;
+        this.password = password;
+        this.email = email;
+        this.roles = roles;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + userName + '\'' +
+                ", lastName='" + password + '\'' +
+                ", age=" + email +
+                '}';
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles;
+    }
+
+    @Override
+    public String getUsername() {
+        return userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ src/main/java/ru/kata/spring/boot_security/demo/initialization/DBInitialization.java
@@ -0,0 +1,51 @@
+package ru.kata.spring.boot_security.demo.initialization;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import ru.kata.spring.boot_security.demo.entityes.Role;
+import ru.kata.spring.boot_security.demo.entityes.User;
+import ru.kata.spring.boot_security.demo.service.RoleService;
+import ru.kata.spring.boot_security.demo.service.UserService;
+
+import javax.annotation.PostConstruct;
+import java.util.Collections;
+
+@Component
+public class DBInitialization {
+    private final UserService userService;
+    private final RoleService roleService;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public DBInitialization(UserService userService, RoleService roleService, PasswordEncoder passwordEncoder) {
+        this.userService = userService;
+        this.roleService = roleService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @PostConstruct
+    public void init() {
+        Role adminRole = new Role();
+        adminRole.setName("ROLE_ADMIN");
+        roleService.addRole(adminRole);
+
+        Role userRole = new Role();
+        userRole.setName("ROLE_USER");
+        roleService.addRole(userRole);
+
+        User admin = new User();
+        admin.setUserName("admin");
+        admin.setPassword(passwordEncoder.encode("admin123"));
+        admin.setEmail("admin@example.com");
+        admin.setRoles(Collections.singleton(adminRole));
+        userService.save(admin);
+
+        User user = new User();
+        user.setUserName("user");
+        user.setPassword(passwordEncoder.encode("user123"));
+        user.setEmail("user@example.com");
+        user.setRoles(Collections.singleton(userRole));
+        userService.save(user);
+    }
+}
